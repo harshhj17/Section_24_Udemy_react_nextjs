38589fd9c7a218eb2fc99232d91c01999aeab6d1
harshhj17 <harshjaiswal857@gmail.com>
2023-08-03 13:16:47 +0530

    section 24

--- 06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
+++ 06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -2,6 +2,7 @@ import { MongoClient, ObjectId } from 'mongodb';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 import {Fragment} from "react";
+import Head from "next/head";
 
 function MeetupDetails(props) {
     return (
@@ -30,10 +31,10 @@ export async function getStaticPaths() {
 
     const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
 
-    client.close();
+  await  client.close();
 
     return {
-        fallback: false,
+        fallback: true,
         paths: meetups.map((meetup) => ({
             params: { meetupId: meetup._id.toString() },
         })),
